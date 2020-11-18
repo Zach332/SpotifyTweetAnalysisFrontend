7a4cbe1284e5430591abb76ba7924934ee7ffd3f
Zachary <heidelzachary@gmail.com>
2020-11-17 23:17:27 -0500

    Centered cards

--- src/Track.js
+++ src/Track.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Track({track}) {
     return (
-        <a href={track.external_urls.spotify} target="_blank" className="list-group-item list-group-item-action m-1 rounded border" style={{width: 250}}>
+        <a href={track.external_urls.spotify} target="_blank" className=" mx-auto list-group-item list-group-item-action m-1 rounded border" style={{width: 250}}>
             <div style={{width: 200}}>
                 <img src={track.album.images[1].url} className="card-img-top"></img>
                 <div className="card-body">
