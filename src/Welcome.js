dc95809deb5f3eb9aee41055ba880e6443c37b67
Sharon Liu <sharonliu138@gmail.com>
2020-11-20 09:33:29 -0500

    fix

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            <h1>Welcome!</h1>
-            <p>This is an awesome website that lets you generate playlists based on the sentiment in recent tweets.</p>
+            <h1>Spotify Tweet Analysis</h1>
+            <p>Generate playlists based on the sentiment in recent tweets.</p>
         </div>
     )
 }
