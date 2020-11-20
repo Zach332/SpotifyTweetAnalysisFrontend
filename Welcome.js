b8425b8d2809e58d415c76ecaeec6954a770357a
Sharon Liu <sharonliu138@gmail.com>
2020-11-20 09:26:02 -0500

    basic ui changes (more to come soon)

---
+++ Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Spotify Tweet Analysis</h1>
+            <p>Generate playlists based on the sentiment in recent tweets.</p>
+        </div>
+    )
+}
